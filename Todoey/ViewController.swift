59511b2700ef8989733677ef72205433da0b0221
Jonatan Vahlberg's Mac <zog-ii@ZOG-IIs-MacBook-Air.local>
2019-03-02 15:08:12 +0100

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by ZOG-II on 2019-03-02.
+//  Copyright © 2019 Jonatan Vahlberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
